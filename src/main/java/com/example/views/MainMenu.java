1bdb4ccafb23641ce04b88e56b39a091b9e64ab9
JuanchiTaveira <juantaveira10@gmail.com>
2024-03-11 01:26:35 +0100

    centrar botones del MainMenu

--- src/main/java/com/example/views/MainMenu.java
+++ src/main/java/com/example/views/MainMenu.java
@@ -10,40 +10,34 @@ public class MainMenu extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	private GestionRestaurante gestionRestaurante;
-
-	/**
+    /**
 	 * Create the panel.
 	 */
 	public MainMenu(GestionRestaurante gestionRestaurante) {
-		this.gestionRestaurante = gestionRestaurante;
 
-		setLayout(new BorderLayout(0, 0));
+        setLayout(new BorderLayout(0, 0));
 		
-		JPanel centerPanel = new JPanel();
-		add(centerPanel, BorderLayout.CENTER);
+		JPanel centerPanel = new JPanel(new GridBagLayout());
 
-		GridBagLayout gbl_centerPanel = new GridBagLayout();
-		gbl_centerPanel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_centerPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_centerPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_centerPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		centerPanel.setLayout(gbl_centerPanel);
+		GridBagConstraints gbc;
+		Insets insets = new Insets(5, 5, 5, 5); // padding
 		
 		JButton btnReservar = new JButton("Reservar");
-		GridBagConstraints gbc_btnReservar = new GridBagConstraints();
-		gbc_btnReservar.insets = new Insets(0, 0, 5, 0);
-		gbc_btnReservar.gridx = 6;
-		gbc_btnReservar.gridy = 2;
-		centerPanel.add(btnReservar, gbc_btnReservar);
-		
-		JButton btnVerReservas = new JButton("Ver Reservas");
-		GridBagConstraints gbc_btnVerReservas = new GridBagConstraints();
-		gbc_btnVerReservas.insets = new Insets(0, 0, 5, 0);
-		gbc_btnVerReservas.gridx = 6;
-		gbc_btnVerReservas.gridy = 4;
-		centerPanel.add(btnVerReservas, gbc_btnVerReservas);
+		gbc = new GridBagConstraints();
+		gbc.insets = insets;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		centerPanel.add(btnReservar, gbc);
 
-		gestionRestaurante.add(this, BorderLayout.CENTER);
+		JButton btnVerReservas = new JButton("Ver Reservas");
+		gbc = new GridBagConstraints();
+		gbc.insets = insets;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		centerPanel.add(btnVerReservas, gbc);
+
+		add(centerPanel, BorderLayout.CENTER);
+
+		gestionRestaurante.getContentPane().add(this, BorderLayout.CENTER);
 	}
 }
