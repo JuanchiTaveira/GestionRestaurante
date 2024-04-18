50d9b6360c94bf3b0b13ce1852fb94a7a14cfd3b
JuanchiTaveira <juantaveira10@gmail.com>
2024-04-18 20:24:40 +0200

    agrego horarioComboBox a PlanoReservar

--- src/main/java/com/example/views/PlanoReservar.java
+++ src/main/java/com/example/views/PlanoReservar.java
@@ -1,15 +1,23 @@
 package com.example.views;
 
 import com.example.GestionRestaurante;
+import com.example.model.Reserva;
 import com.toedter.calendar.JCalendar;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
 
 public class PlanoReservar extends JPanel {
 
@@ -25,16 +33,28 @@ public class PlanoReservar extends JPanel {
 		JCalendar calendar = new JCalendar();
 		calendar.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+		JComboBox horarioComboBox = new JComboBox();
+		horarioComboBox.setModel(new DefaultComboBoxModel(Reserva.Horario.values()));
+		BasicComboBoxRenderer basicComboBoxRenderer = new BasicComboBoxRenderer();
+		basicComboBoxRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+		horarioComboBox.setRenderer(basicComboBoxRenderer);
+
 		GroupLayout gl_calendarContainer = new GroupLayout(calendarContainer);
 		gl_calendarContainer.setHorizontalGroup(
 			gl_calendarContainer.createParallelGroup(Alignment.LEADING)
 				.addComponent(calendar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addGroup(Alignment.TRAILING, gl_calendarContainer.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(horarioComboBox, 0, 184, Short.MAX_VALUE)
+					.addContainerGap())
 		);
 		gl_calendarContainer.setVerticalGroup(
 			gl_calendarContainer.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_calendarContainer.createSequentialGroup()
 					.addComponent(calendar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(305, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(horarioComboBox, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(241, Short.MAX_VALUE))
 		);
 
 		calendarContainer.setLayout(gl_calendarContainer);
