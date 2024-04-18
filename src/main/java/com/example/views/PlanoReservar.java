30e9bcbb29d18a818d83b64db2c7cd4bc95b37e7
JuanchiTaveira <juantaveira10@gmail.com>
2024-04-18 20:13:20 +0200

    mejora estetica a PlanoReservar

--- src/main/java/com/example/views/PlanoReservar.java
+++ src/main/java/com/example/views/PlanoReservar.java
@@ -3,13 +3,11 @@ package com.example.views;
 import com.example.GestionRestaurante;
 import com.toedter.calendar.JCalendar;
 
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
+import javax.swing.border.EmptyBorder;
 
 public class PlanoReservar extends JPanel {
 
@@ -19,11 +17,11 @@ public class PlanoReservar extends JPanel {
 		setLayout(new BorderLayout(0, 0));
 
 		JPanel calendarContainer = new JPanel(null);
-		calendarContainer.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		calendarContainer.setBorder(new EmptyBorder(10, 10, 10, 10));
 		add(calendarContainer, BorderLayout.WEST);
 
 		JCalendar calendar = new JCalendar();
-		calendar.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		calendar.setBorder(null);
 
 		GroupLayout gl_calendarContainer = new GroupLayout(calendarContainer);
 		gl_calendarContainer.setHorizontalGroup(
@@ -32,7 +30,9 @@ public class PlanoReservar extends JPanel {
 		);
 		gl_calendarContainer.setVerticalGroup(
 			gl_calendarContainer.createParallelGroup(Alignment.LEADING)
-				.addComponent(calendar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addGroup(gl_calendarContainer.createSequentialGroup()
+					.addComponent(calendar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(305, Short.MAX_VALUE))
 		);
 
 		calendarContainer.setLayout(gl_calendarContainer);
