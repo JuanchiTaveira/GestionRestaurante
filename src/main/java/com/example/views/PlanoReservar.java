d480b39d1f73998505ee88120414d0984f5546db
JuanchiTaveira <juantaveira10@gmail.com>
2024-04-18 19:21:05 +0200

    agrego JCalendar y pantalla planoReservar

--- src/main/java/com/example/views/PlanoReservar.java
+++ src/main/java/com/example/views/PlanoReservar.java
@@ -1,10 +1,13 @@
 package com.example.views;
 
 import com.example.GestionRestaurante;
-import com.toedter.calendar.JDateChooser;
+import com.toedter.calendar.JCalendar;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 public class PlanoReservar extends JPanel {
 
@@ -13,8 +16,17 @@ public class PlanoReservar extends JPanel {
 	public PlanoReservar(GestionRestaurante gestionRestaurante) {
 		setLayout(new BorderLayout(0, 0));
 
-		JDateChooser dateChooser = new JDateChooser();
-		add(dateChooser, BorderLayout.WEST);
+		JPanel calendarContainer = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0; // en la columna cero
+		gbc.gridy = 0; // en la fila cero
+		gbc.anchor = GridBagConstraints.NORTH;
+		gbc.insets = new Insets(10, 10, 10, 10); // márgenes para el calendario
+
+		JCalendar calendar = new JCalendar();
+		calendarContainer.add(calendar, gbc);
+
+		add(calendarContainer, BorderLayout.WEST);
 
 		gestionRestaurante.getContentPane().add(this, BorderLayout.CENTER);
 		gestionRestaurante.setSize(800,500);
