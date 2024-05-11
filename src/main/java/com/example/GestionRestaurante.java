6d5ff62358b40411248fc0a4709f1d37d0b321b2
JuanchiTaveira <juantaveira10@gmail.com>
2024-05-11 20:12:32 +0200

    poblar bbdd

--- src/main/java/com/example/GestionRestaurante.java
+++ src/main/java/com/example/GestionRestaurante.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.controller.ClienteController;
 import com.example.controller.EmpleadoController;
 import com.example.controller.MesaController;
 import com.example.controller.ReservaController;
@@ -7,11 +8,13 @@ import com.example.model.Empleado;
 import com.example.model.Reserva;
 import com.example.model.Cliente;
 import com.example.views.LoginPanel;
+import com.github.javafaker.Faker;
 
 import javax.swing.JFrame;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class GestionRestaurante extends JFrame {
 
@@ -29,7 +32,6 @@ public class GestionRestaurante extends JFrame {
 	public GestionRestaurante() {
 		crearMesas();
 		insertTestData();
-
 		initialize();
 	}
 
@@ -43,6 +45,8 @@ public class GestionRestaurante extends JFrame {
 	private void insertTestData() {
 		EmpleadoController empleadoController = new EmpleadoController();
 		ReservaController reservaController = new ReservaController();
+		MesaController mesaController = new MesaController();
+		ClienteController clienteController = new ClienteController();
 
 		Empleado empleado = new Empleado("admin", "admin", "Admin", "Admin", "12345678", true);
 		Empleado empleado1 = new Empleado("juan", "juan", "Juan", "Taveira", "Y123456Z", false);
@@ -53,6 +57,42 @@ public class GestionRestaurante extends JFrame {
 		reservaController.insertarReserva(new Reserva(new Cliente("Juan", "Taveira", "+34112233", "juan@juan.com"), 1, LocalDate.now(), Reserva.Horario.ALMUERZO, 5, empleado));
 		reservaController.insertarReserva(new Reserva(new Cliente("Leo", "Messi", "+34112233", "messi@messi.com"), 5, LocalDate.now(), Reserva.Horario.ALMUERZO, 4, empleado2));
 		reservaController.insertarReserva(new Reserva(new Cliente("Asier", "Azpiazu", "+34666000666", "asier@gmail.com"), 2, LocalDate.now(), Reserva.Horario.ALMUERZO, 3, empleado1));
+
+		Faker faker = new Faker(new Locale("es"));
+
+		// Crear empleados
+		for (int i = 0; i < 10; i++) {
+			String usuario = faker.name().username();
+			String password = faker.internet().password();
+			String nombre = faker.name().firstName();
+			String apellido = faker.name().lastName();
+			String dni = faker.idNumber().valid();
+			boolean admin = i == 0; // El primer empleado será admin
+
+			empleadoController.insertarEmpleado(new Empleado(usuario, password, nombre, apellido, dni, admin));
+		}
+
+		// Crear reservas
+		for (int i = 0; i < 50; i++) {
+			String clienteNombre = faker.name().firstName();
+			String clienteApellido = faker.name().lastName();
+			String clienteTelefono = faker.phoneNumber().phoneNumber();
+			String clienteCorreo = faker.internet().emailAddress();
+			Cliente cliente = new Cliente(clienteNombre, clienteApellido, clienteTelefono, clienteCorreo);
+			clienteController.insertarClienteReserva(cliente);
+
+			int numeroMesa = faker.number().numberBetween(1, 13);
+			LocalDate dia = LocalDate.now().plusDays(faker.number().numberBetween(0, 5));
+			Reserva.Horario horario = faker.options().option(Reserva.Horario.class);
+
+			int cantidadPersonas = faker.number().numberBetween(1, mesaController.maxPersonasMesa(numeroMesa));
+
+			// Asignar un empleado al azar a la reserva
+			Empleado empleadoRandom = empleadoController.getEmpleadoById(String.valueOf(faker.number().numberBetween(1, 10)));
+
+			Reserva reserva = new Reserva(cliente, numeroMesa, dia, horario, cantidadPersonas, empleadoRandom);
+			reservaController.insertarReserva(reserva);
+		}
 	}
 
 	private void crearMesas() {
